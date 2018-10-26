b74b63cc52bd6cd69577a4713cb2c7d676e8651a
Loco9 <codylj@rose-hulman.edu>
2018-10-25 21:21:10 -0400

    DONE

--- src/problem1.py
+++ src/problem1.py
@@ -2,9 +2,9 @@
 Exam 3, problem 1.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  October 2018.
+         their colleagues and Logan Cody.  October 2018.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -86,10 +86,15 @@ def problem1(numbers):
       :type numbers:  [int]
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
 
+    count = 0
+    a = len(numbers) // 3
+    for k in range(a):
+        count = count + numbers[k + a]
+    return count
 
 ###############################################################################
 # Our tests use the following to print error messages in red.
